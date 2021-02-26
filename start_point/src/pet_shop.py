d84397401cefdfb190b0d1f5bcf256e6e0fce0c9
Piotr <pigryko@yahoo.com>
2021-02-26 23:33:23 +0000

    First 11 tests done

--- start_point/src/pet_shop.py
+++ start_point/src/pet_shop.py
@@ -1,1 +1,44 @@
 # WRITE YOUR FUNCTIONS HERE
+
+# Pet shop name
+def get_pet_shop_name(list):
+    return list["name"]
+
+
+
+# Total cash
+def get_total_cash(list):
+    return list["admin"]["total_cash"]
+
+# Add or remove cash
+def add_or_remove_cash(list, add_amount):
+    list["admin"]["total_cash"] += add_amount
+
+
+# Pets sold
+def get_pets_sold(list):
+    return list["admin"]["pets_sold"]
+
+# Increase pets sold
+def increase_pets_sold(list,pets_sold_amount):
+    list["admin"]["pets_sold"] += pets_sold_amount
+
+# Stock count
+def get_stock_count(list):
+    return len(list["pets"])
+
+# Get pets by breed
+def get_pets_by_breed(list, breed_name):
+    pets_by_breed = []
+    for pet in list["pets"]:
+        if pet["breed"] == breed_name:
+            pets_by_breed.append(1)
+    return pets_by_breed
+
+# Find pet by name
+def find_pet_by_name(list, pet_name):
+    pet_by_name = None
+    for pet in list["pets"]:
+        if pet["name"] == pet_name:
+            pet_by_name = pet
+    return pet_by_name
---
+++ start_point/temp_test.py
@@ -0,0 +1,61 @@
+pet_shop = {
+            "pets": [
+                {
+                    "name": "Sir Percy",
+                    "pet_type": "cat",
+                    "breed": "British Shorthair",
+                    "price": 500
+                },
+                {
+                    "name": "King Bagdemagus",
+                    "pet_type": "cat",
+                    "breed": "British Shorthair",
+                    "price": 500
+                },
+                {
+                    "name": "Sir Lancelot",
+                    "pet_type": "dog",
+                    "breed": "Pomsky",
+                    "price": 1000,
+                },
+                {
+                    "name": "Arthur",
+                    "pet_type": "dog",
+                    "breed": "Husky",
+                    "price": 900,
+                },
+                {
+                    "name": "Tristan",
+                    "pet_type": "cat",
+                    "breed": "Basset Hound",
+                    "price": 800,
+                },
+                {
+                    "name": "Merlin",
+                    "pet_type": "cat",
+                    "breed": "Egyptian Mau",
+                    "price": 1500,
+                }
+            ],
+            "admin": {
+                "total_cash": 1000,
+                "pets_sold": 0,
+            },
+            "name": "Camelot of Pets"
+        }
+
+
+
+def get_pets_by_breed(list, breed_name):
+
+    pets_by_breed = []
+
+    for pet in list["pets"]:
+        if list["pets"][0]["breed"] == breed_name:
+            pets_by_breed.append(1)
+    return pets_by_breed
+    print(pets_by_breed)
+
+get_pets_by_breed(pet_shop, "Husky")
+
+print (pet_shop["pets"][5])
